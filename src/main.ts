7d4754739d05ed6c84a2b18d9e4db15dd192b152
Joosung Park <admin@slowmotion.dev>
2019-11-21 22:46:23 +0900

    Add: NO CORS

--- src/main.ts
+++ src/main.ts
@@ -17,6 +17,9 @@ if (config.apm) {
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+  });
 
   const options = new DocumentBuilder()
     .setTitle('Mealy')
